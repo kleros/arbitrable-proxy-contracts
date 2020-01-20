edfe1af7affcac70dae7f9e58e39369a5b4b7d58
Ferit Tunçer <ferit@cryptolab.net>
2020-01-21 02:15:02 +0300

    feat(migrations): deploy bap

---
+++ migrations/2_deploy_bap.js
@@ -0,0 +1,20 @@
+const BAP = artifacts.require("BinaryArbitrableProxy");
+
+module.exports = function(deployer, network) {
+  if (network == "main")
+    deployer.deploy(
+      BAP,
+      "0x988b3a538b618c7a603e1c11ab82cd16dbe28069", // KlerosLiquid on Main
+      "10000",
+      "10000",
+      "10000"
+    );
+  else if (network == "kovan")
+    deployer.deploy(
+      BAP,
+      "0x60B2AbfDfaD9c0873242f59f2A8c32A3Cc682f80", // KlerosLiquid on Kovan
+      "10000",
+      "10000",
+      "10000"
+    );
+};
