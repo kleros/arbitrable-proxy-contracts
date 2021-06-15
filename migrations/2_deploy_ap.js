647ea6e38cbcde963f0e762144afbf22bf9ae12d
Ferit Tunçer <ferit@cryptolab.net>
2021-06-15 21:04:22 +0300

    refactor(deploy-ap): change sokol kleros liquid address

--- migrations/2_deploy_ap.js
+++ migrations/2_deploy_ap.js
@@ -4,7 +4,7 @@ const KLEROS = {
   main: "0x988b3a538b618c7a603e1c11ab82cd16dbe28069",
   kovan: "0x60B2AbfDfaD9c0873242f59f2A8c32A3Cc682f80",
   ropsten: "0xab942E7D2bfF0bc5614c968ccc91198fD223C57E",
-  sokol: "0xec8726b782D77fC9Fdf64d12D895CA15956b9e0e",
+  sokol: "0xb701ff19fBD9702DD7Ca099Ee7D0D42a2612baB5",
 };
 
 module.exports = function (deployer, network) {
