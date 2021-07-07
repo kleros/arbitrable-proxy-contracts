9483c24bd199df0cad9228beb502edc34c906672
Ferit Tunçer <0xferittuncer@gmail.com>
2021-07-07 17:15:01 +0300

    refactor(migrations): add poa kleros liquid

--- migrations/2_deploy_ap.js
+++ migrations/2_deploy_ap.js
@@ -5,6 +5,7 @@ const KLEROS = {
   kovan: "0x60B2AbfDfaD9c0873242f59f2A8c32A3Cc682f80",
   ropsten: "0xab942E7D2bfF0bc5614c968ccc91198fD223C57E",
   sokol: "0xb701ff19fBD9702DD7Ca099Ee7D0D42a2612baB5",
+  poa: "0x9C1dA9A04925bDfDedf0f6421bC7EEa8305F9002",
 };
 
 module.exports = function (deployer, network) {
